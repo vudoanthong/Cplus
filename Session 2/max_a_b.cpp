6d94eccc7b8bc7031d28217b046dc90385878db4
vudoanthong <vudoanthong@gmail.com>
2018-04-27 14:28:42 +0700

    Session2_Câu lệnh điều khiển
    Lý thuyết trong file Ly_thuyet_Session2.txt
    Hướng dẫn trong file
    Huong_dan_Session2.txt

---
+++ Session 2/max_a_b.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+include<conio>
+using namepace std;
+int main()
+	{
+		int a,b,max;
+		cout <<"Nhap a: ";
+		cin >> a;
+		cout << "Nhap b: ";
+		con >> b;
+		max = b;
+		if(a>b)
+			max = a;
+		cout << max << "là so lon nhat.";
+		return 0;
+	}
